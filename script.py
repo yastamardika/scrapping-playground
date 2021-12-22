0816c92b5d9fd01621bec2d6e2e3b5fd333020f9
yastamardika <rattyasta1@gmail.com>
2021-12-23 02:05:42 +0700

    separate function and multi page support

--- script.py
+++ script.py
@@ -15,63 +15,53 @@ all_sanggars = []
 all_images = []
 sanggars = []
 img = []
+def scrpMap(sanggars, img):
+    image_srcs = [x['src'] for x in img]
+    for image in image_srcs:
+        all_images.append({
+            "img": image
+        })
+
+    for sanggar, image_src in zip(sanggars, image_srcs):
+        name = sanggar.select('td')[2].text.strip()
+        address = sanggar.select('td')[3].text.strip()
+        postal = sanggar.select('td')[4].text.strip()
+        phone = sanggar.select('td')[5].text.strip()
+        mail = sanggar.select('td')[7].text.strip()
+        service = sanggar.select('td')[9].text.strip()
+        
+        all_sanggars.append({
+            "name" : name,
+            "address" : address,
+            "postal" : postal,
+            "phone" : phone,
+            "mail" : mail,
+            "service" : service,
+            "image" : image_src
+            })
+            
+    print(all_sanggars)
+    checkout(all_sanggars)
+
+def checkout(all_sanggars):
+    keys = all_sanggars[0].keys()
+    with open('sanggar.csv', 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(all_sanggars)
+
+    with open('sanggar.json','w') as output_file:
+        json.dump(all_sanggars, output_file, indent=8, ensure_ascii = False)
+        print('JSON Exported!')
+
 while True:
     sanggars = soup.find(id="grid_block1").select('table > tbody > tr')
     img = soup.find(id="grid_block1").select('table > tbody > tr > td > a > img', src=True)
-    images = soup.find_all('img', src=True)
+    scrpMap(sanggars, img)
     page = 1
     try:
         page = page + 1
         driver.find_element_by_xpath("//a[contains(@href, 'JavaScript:GotoPage1("+str(page)+");')]").click()
         soup = BeautifulSoup(driver.page_source, 'html.parser')
-        sanggars = soup.find(id="grid_block1").select('table > tbody > tr')
-        img = soup.find(id="grid_block1").select('table > tbody > tr > td > a > img', src=True)
-        print(sanggars)
+        # print(sanggars)
     except Exception: break
-# for imgs in img:
-#     print(imgs)
-
-
-# for image in images:
-#     print(image)
-# print(img)
-# for pics in img: 
-#     print(pics)
-image_srcs = [x['src'] for x in img]
-for image in image_srcs:
-    all_images.append({
-        "img": image
-    })
-
-i = 0
-for sanggar, image_src in zip(sanggars, image_srcs):
-    name = sanggar.select('td')[2].text.strip()
-    address = sanggar.select('td')[3].text.strip()
-    postal = sanggar.select('td')[4].text.strip()
-    phone = sanggar.select('td')[5].text.strip()
-    mail = sanggar.select('td')[7].text.strip()
-    service = sanggar.select('td')[9].text.strip()
-    
-    all_sanggars.append({
-        "id" : i,
-        "name" : name,
-        "address" : address,
-        "postal" : postal,
-        "phone" : phone,
-        "mail" : mail,
-        "service" : service,
-        "image" : image_src
-        })
-    i = i+1
-
-print(all_sanggars)
-keys = all_sanggars[0].keys()
-
-# with open('sanggar.csv', 'w', newline='') as output_file:
-#     dict_writer = csv.DictWriter(output_file, keys)
-#     dict_writer.writeheader()
-#     dict_writer.writerows(all_sanggars)
-
-# with open('sanggar.json','w') as output_file:
-#     json.dump(all_sanggars, output_file, indent=8, ensure_ascii = False)
-#     print('JSON Exported!')
